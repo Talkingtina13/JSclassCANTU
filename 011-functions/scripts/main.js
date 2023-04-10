2a3abdd8396888489a40b4dadf259d62cbd3166f
Christina Cantu <111158329+Talkingtina13@users.noreply.github.com>
2023-04-10 13:26:33 -0700

    regular functions

--- 011-functions/scripts/main.js
+++ 011-functions/scripts/main.js
@@ -9,10 +9,17 @@ function doMath (a,b) {
    // console.log(`this is the result ${a * b}`)
 }
 
-doMoreMath = (a, b) => a + b
+
+doMoreMath = (a,b) => a * b 
+divide = (a,b) => a / b
+max = (a,b) => a - b
   
 
-console.log(doMoreMath(20 ,30))
+console.log(doMoreMath(75 ,30))
+
+console.log(divide(650,65))
+
+console.log(subtract(15,8))
 
 const result = doMath(10, 4)
 console.log(`this result was returned from the function  ${result}`)
