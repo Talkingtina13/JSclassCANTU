64c4a66ea77807c4cefb5a84f8e66ab798915c58
Christina Cantu <111158329+Talkingtina13@users.noreply.github.com>
2023-04-17 12:18:53 -0700

    updating the DOM

---
+++ 012-start-building/scripts/main.js
@@ -0,0 +1,13 @@
+// update the dom
+
+const updateDom = (input, targetId = 'output') => {
+  const divEl = document.querySelector(`#${targetId}`)
+  const p = document.createElement('p')
+  p.textContent = input
+  divEl.appendChild(p)
+
+}
+
+
+updateDom("Hello, there")
+//updateDom("Hello, there")
