c1796c7dc95bbc2d6c6c65abcc5e33c30bc6c0d1
Christina Cantu <111158329+Talkingtina13@users.noreply.github.com>
2023-05-11 11:59:36 -0700

    This is mine

--- 017-intro-to-objects/main.js
+++ 017-intro-to-objects/main.js
@@ -1,22 +1,35 @@
-const myObj = {}
 const myArr = []
 
 //console.log(typeof myObj)
 //console.log(typeof myArr)
 //console.log(myArr === myObj)
-const myNumber = 9
-const myObj ={
-    name: 'Tina',
-    age: 23,
-    hobbies: ['art', 'kickboxing'],
+const myNumber = 13
+const myObj = {
+    name: 'Rio',
+    age: 59,
+    hobbies: ['coding', 'running', 'workout'],
     mobile: {
-        make: 'iPhone 13',
-        OS: 'ios 16'
+        make: 'pixel 7', 
+        OS: 'android',
     },
-    myGreeting: function(person) {
-        return `${this.name} who loves to ${this.hobbies[0]} says What is UP ${person}`
+    car: {
+        make: 'Toyota',
+        model: 'Prius',
+        year: '2004',
     },
+    pets: ['Forest', 'Karl','Champ'],
+
+    myStatement: function(person, age) {
+        if (age === 23) {
+            console.log(`${person} is the same age as ${this.name}`)
+        } else if (age > 23) {
+            console.log(`${person} is older than ${this.name}`)
+        } else {
+            console.log(`${person} is younger than ${this.name}`)
+        }
+    },
+
 }
 
-const greeting = myObj.myGreeting('jane')
-console.log(greeting)
+const myHi = myObj.myStatement('kim', 27)
+console.log(myHi)
