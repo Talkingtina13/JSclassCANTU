fdddc73dfb25b8de9df35dd622793589ee0e2f19
Christina Cantu <lovelycantu@gmail.com>
2023-05-12 15:22:35 -0700

    table creation successful

--- 021-creating-table-w-code/main.js
+++ 021-creating-table-w-code/main.js
@@ -13,73 +13,98 @@ function updateDOM (input, id) {
 }
 
 function trackMPGandCost (miles, gallons, price) {
-    const MPG  = Math.round(miles/gallons)
+    const MPG = Math.round(miles/gallons)
     const tripCost = Math.round(gallons * price)
-    updateDOM(`Miles per gallon  is ${MPG} and trip cost is ${tripCost}`, '#output')
-    obj.MPG = MPG
-    obj.tripCost = tripCost
-   
+    updateDOM(`Miles per gallon is ${MPG} and trip cost is ${tripCost}`, '#output')
     return {
-        MPG: MPG, 
-        tripCost: tripCost,
         miles: miles,
         gallons: gallons,
-        price: price
+        price: price,
+        MPG: MPG,
+        tripCost: tripCost,
     }
 }
 
-function calculateAvg () {
-    const numberOfObj = MY_DATA.length
-    let sumMPG  = 0
-    let sumTripCost = 0  
+function calculateAvg() {  
+    let sumMPG = 0
+    let sumTripCost = 0
     MY_DATA.forEach(obj => {
         sumMPG += obj.MPG
         sumTripCost += obj.tripCost
     })
-    const avgMPG = Math.round(sumMPG/numberOfObj)
-    const avgTripCost = Math.round(sumTripCost/numberOfObj)
+    let avgMPG = Math.round(sumMPG/MY_DATA.length)
+    let avgTripCost = Math.round(sumTripCost/MY_DATA.length)
     updateDOM(`Average MPG is ${avgMPG}`, '#output-avg')
-    updateDOM(`Average Trip Cost is ${avgTripCost}`, '#output-avg')
+    updateDOM(`Average trip cost is ${avgTripCost}`, '#output-avg')
 }
 
-function isFormValid  (miles, gallons, price) {
+function isFormValid (miles, gallons, price) {
     const errMsg = []
-    if (miles === 0 || gallons === 0 || price === 0) {
-        errMsg.push('Make sure your input value greater than 0!!, Try Again')
-    }
+    if(miles === 0 || gallons === 0 || price === 0) {
+        errMsg.push('Make sure you input value greater than 0')
+    } 
     if (price > 1000) {
-        errMsg.push('Really!!!?? I think this is in error...Try again')
+        errMsg.push('Really? I think this is an error...try again')
     }
-    if (errMsg.length > 0) {
+    if(errMsg.length > 0) {
         ERR.textContent = errMsg
-        return false
+    
     } else {
         return true
-
     }
-}
+} 
 
-function renderTable(){
+
+function renderTableHeadings() {
     const tbl = document.createElement('table')
-    const headings = ['Miles Driven:','Gallons Used:','Price Paid:','Trip MPG','Trip Cost','Edit/Delete']
+    const headings = ['Miles Drive:', 'Gallons Used:', 'Price Paid:', 'Trip MPG', 'Trip Cost', 'Edit/Delete']
     const tr = document.createElement('tr')
-    headings.forEach (function(heading){
+    headings.forEach(function(heading) {
         let th = document.createElement('th')
         th.textContent = heading
         tr.appendChild(th)
     })
-    console.log (tr)
-    tbl.appendChild (tr)
-    TBL_OUTPUT.appendChild(tbl)
+    tbl.appendChild(tr)
+    return tbl
 }
 
-MY_DATA.forEach (function (obj) {
-    const tr = document.createElement('tr')
-    for (key in obj){
-      let td = document.createElement('td')
-        td.textContent = obj[key]
-        tr.appendChild (td)
-    }
-    tbl.appendChild (tr)
-    })
+function renderTable() {
+    const tbl = renderTableHeadings()
+    TBL_OUTPUT.appendChild(tbl)
+    MY_DATA.forEach(function(obj){
+        const tr = document.createElement('tr')
+        for (key in obj) {
+            let td = document.createElement('td')
+            td.textContent = obj[key]
+            tr.appendChild(td)
+        }
 
+        const btnTD = document.createElement('td'
+        )
+        const editBtn = document.createElement('button')
+        editBtn.textContent = 'edit'
+        const delBtn = document.createElement('button')
+        delBtn.textContent = 'delete'
+        btnTD.appendChild(editBtn)
+        btnTD.appendChild(delBtn)
+        tr.appendChild(btnTD)
+        tbl.appendChild(tr)
+    })
+}
+
+FORM.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const miles = parseInt(e.target.miles.value)
+    const gallons = parseInt(e.target.gallons.value)
+    const price = parseInt(e.target.price.value)
+    const isValid = isFormValid(miles, gallons, price)
+    if(isValid) {       
+        ERR.textContent = ''
+        AVG_OUTPUT.textContent = ''
+        const dataObj = trackMPGandCost(miles, gallons, price)
+        MY_DATA.push(dataObj)
+        renderTable()
+        calculateAvg()
+    }
+    FORM.reset()
+})
