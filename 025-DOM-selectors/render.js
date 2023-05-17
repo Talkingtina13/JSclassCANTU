aa1babb8cefb95dbfaab5bade96c931f3a808e5c
Christina Cantu <lovelycantu@gmail.com>
2023-05-17 14:12:56 -0700

    selectors and code challenge

--- 025-DOM-selectors/render.js
+++ 025-DOM-selectors/render.js
@@ -36,6 +36,8 @@ function renderEditDelBtn(MY_DATA, index) {
         MY_DATA.splice(index, 1)
         saveTripData(MY_DATA)
         renderTable(MY_DATA)
+        const button = document.querySelector("Button"); 
+        button.setAttribute("disabled", "disabled");
     })
 
     td.appendChild(editBtn);
